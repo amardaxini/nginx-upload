fdd60fde31df4139622d2a6673a68719f54f7b85
amardaxini <amardaxini@gmail.com>
2011-03-08 13:19:42 +0530

    added description migration

---
+++ db/migrate/20110308062237_add_description_to_videos.rb
@@ -0,0 +1,9 @@
+class AddDescriptionToVideos < ActiveRecord::Migration
+  def self.up
+    add_column :videos, :description, :text
+  end
+
+  def self.down
+    remove_column :videos, :description
+  end
+end
